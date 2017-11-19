a8dad6820e60ec4e1acc7754428c82f6e6553001
Sergei Shvakel <SergeiSh@tut.by>
2017-11-19 19:40:44 +0300

    v.1.0.0.3

--- TestReadFile/FileReadStream.h
+++ TestReadFile/FileReadStream.h
@@ -8,16 +8,34 @@ public:
 	FileReadStream(const TCHAR *lpszFileName, unsigned int nSizeCash);
 	virtual ~FileReadStream();
 
+	void init(int nBlockSize);
+
+	ReadStreamStatus getStatus();
+
 	void startRead();
+	void stopRead();
 	void readBlock();
+
+	unsigned int
+	getBuffer(unsigned char *pcopyto, unsigned int uSize);
+
+	void flushBuff();
 
 private:
 	TCHAR m_FileName[FILENAME_MAX + 1];
 
+	HANDLE m_handleFile;
+	OVERLAPPED m_overlapped;
+	DWORD m_dwBytesRead;
+
 	unsigned int	m_SizeCash;
 	unsigned char	*m_BuffCash;
+	unsigned int    m_nCurPos;
+
+	int m_nBlockSize;
 
 	ReadStreamStatus m_Status;
 	
+	void clearCashBuff();
 };
 
--- TestReadFile/IReadStream.h
+++ TestReadFile/IReadStream.h
@@ -1,13 +1,4 @@
 #pragma once
-class IReadStream
-{
-public:
-
-	virtual void startRead() = 0;
-	virtual void readBlock() = 0;
-
-	virtual ~IReadStream() {};
-};
 
 enum ReadStreamStatus
 {
@@ -15,4 +6,43 @@ enum ReadStreamStatus
 	STREAM_READ,
 	STREAM_WAIT,
 	STREAM_BUFFFULL,
+	STREAM_END,
+};
+
+typedef
+int IREADSTREAMEXCEPTION;
+
+enum ReadStreamError
+{
+	STREAMERR_SUCCESS			= 0,
+
+	STREAMERR_INVALIDPARAM		= -1,
+	STREAMERR_INCORRECTSTATUS	= -2,
+	STREAMERR_ALLOCMEM			= -3,
+	STREAMERR_NOTINITYET		= -4,
+
+	STREAMERR_OPENSTREAM		= -5,
+	STREAMERR_READSTREAM		= -6,
+
+	STREAMERR_NOTENOUGH			= -7,
+};
+
+#define READSTREAM_MAXBLOCKSIZE 1024
+
+class IReadStream
+{
+public:
+
+	virtual void init(int nBlockSize) = 0;
+
+	virtual ReadStreamStatus getStatus() = 0;
+
+	virtual void startRead() = 0;
+	virtual void stopRead() = 0;
+	virtual void readBlock() = 0;
+
+	virtual unsigned int getBuffer(unsigned char *pcopyto, unsigned int uSize) = 0;
+	virtual void flushBuff() = 0;
+
+	virtual ~IReadStream() {};
 };
